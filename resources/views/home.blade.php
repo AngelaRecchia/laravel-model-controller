e3a55c49f37266b74f55c0b3702ee27bc24b8010
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-09-11 11:16:59 +0200

    wip comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,20 @@
+<!-- homepage = comics page -->
+
 @extends('layout.app')
 @section('title', 'DC Comics')
 @section('content')
-    <h1>MAIN</h1>
+    <section id="comics">
+        <div class="container">
+            <div class="button-big bold halfway">Current Series</div>
+            <div class="comics-cont">
+                @foreach($comics as $comic)
+                <div class="comic">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                </div>
+                @endforeach
+            </div>
+        </div>
+        
+    </section>
+    
 @endsection
